9f934163a279843a9ba9ac4dac12ae8135d7b005
Jono Gilmour <j.gilmour@webtron.com.au>
2018-06-25 22:53:33 +1000

    Added handler for POST to users - creates a new user. Also refactored some files, moving config to the lib dir.

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
 var fs = require('fs');
 var handlers = require('./lib/handlers');
-
+var helpers = require('./lib/helpers');
 
 // HTTP server
 var httpServer = http.createServer(function(req, res) {
@@ -74,7 +74,7 @@ var unifiedServer = function(req, res) {
       'queryStringObject' : queryStringObject,
       'method' : method,
       'headers' : headers,
-      'payload' : buffer
+      'payload' : helpers.parseJsonToObject(buffer)
     };
 
     // Route the request to the handler specified in the router
@@ -99,19 +99,6 @@ var unifiedServer = function(req, res) {
 
   });
 
-};
-
-// Define the handlers
-var handlers = {};
-
-// Ping handler
-handlers.ping = function(data, callback) {
-  callback(200);
-}
-
-// Not found handler
-handlers.notFound = function(data, callback) {
-  callback(404);
 };
 
 // Define a request router

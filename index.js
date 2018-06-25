1618370016dd43eead61d45141e9a86d9e39ce20
Jono Gilmour <j.gilmour@webtron.com.au>
2018-06-25 14:06:53 +1000

    Implemented payloads

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
@@ -26,11 +27,21 @@ var server = http.createServer(function(req, res) {
   // Get the headers as an object
   var headers = req.headers;
 
-  // Send the response
-  res.end('Hello world\n');
+  // Get the payload if present
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', function() {
+    buffer += decoder.end();
 
-  // Log the request path
-  console.log('Request received with headers',headers);
+    // Send the response
+    res.end('Hello world\n');
+
+    // Log the request path
+    console.log('Request received with payload: ',buffer);
+  });
 });
 
 // Start the server and listen on port 3000

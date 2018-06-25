0665bc0ef13f25a67a2858c6e4315818f8140015
Jono Gilmour <j.gilmour@webtron.com.au>
2018-06-25 13:30:17 +1000

    Server logs the canonical path to the console

--- index.js
+++ index.js
@@ -5,10 +5,23 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
+
+  // Get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the url's path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
   res.end('Hello world\n');
+
+  // Log the request path
+  console.log('Requested received on path: '+trimmedPath);
 });
 
 // Start the server and listen on port 3000

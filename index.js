4f0779bc30fe98cad2d6426011ecf20d0430c89b
Jono Gilmour <j.gilmour@webtron.com.au>
2018-06-25 13:51:14 +1000

    Added headers logging

--- index.js
+++ index.js
@@ -23,11 +23,14 @@ var server = http.createServer(function(req, res) {
   // Get the http method
   var method = req.method.toLowerCase();
 
+  // Get the headers as an object
+  var headers = req.headers;
+
   // Send the response
   res.end('Hello world\n');
 
   // Log the request path
-  console.log('Requested received on path: '+trimmedPath+' with method: '+method+' and with these query string params: ',queryStringObject);
+  console.log('Request received with headers',headers);
 });
 
 // Start the server and listen on port 3000

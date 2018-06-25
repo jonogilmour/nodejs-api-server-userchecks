d4e6c66da567c428eefa7a29d445bad27840f2a7
Jono Gilmour <j.gilmour@webtron.com.au>
2018-06-25 13:09:57 +1000

    Basic server setup, responds with Hello world

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world!");
+/*
+ * Primary API file
+ *
+ */
+
+// Dependencies
+var http = require('http');
+
+// The server should respond to all requests with a string
+var server = http.createServer(function(req, res) {
+  res.end('Hello world\n');
+});
+
+// Start the server and listen on port 3000
+server.listen(3000, function() {
+  console.log('Server is listening on port 3000');
+});

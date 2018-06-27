e2321ead80eda10d16a20a3dda06b37659bc4c5f
Jono Gilmour <j.gilmour@webtron.com.au>
2018-06-27 16:32:41 +1000

    Added .data to ignore. Added POST /tokens for token creation

--- index.js
+++ index.js
@@ -104,5 +104,6 @@ var unifiedServer = function(req, res) {
 // Define a request router
 var router = {
   'ping' : handlers.ping,
-  'users' : handlers.users
+  'users' : handlers.users,
+  'tokens' : handlers.tokens
 };

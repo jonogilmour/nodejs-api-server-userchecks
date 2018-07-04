1ac0bcc9d0596e2ecb8096e0dc2fa18d5a755867
Jono Gilmour <j.gilmour@webtron.com.au>
2018-07-04 22:36:01 +1000

    Full ES6 and pure functional rewrite of helpers.js

--- index.js
+++ index.js
@@ -6,24 +6,6 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
-const _data = require('./lib/data');
-
-_data.list('tokens', (err, data) => {
-  console.log(err, data);
-});
-_data.create('test', 'test1.txt', 'This is a test', (err) => {
-  if(!err) {
-    _data.read('test', 'test1.txt', (err, data) => {
-      if(!err && data) {
-        _data.delete('test', 'test1.txt', (err) => {
-          console.log('delete', err);
-        });
-      }
-      console.log('read', err, data);
-    })
-  }
-  console.log('create', err);
-});
 
 // Declare the app
 const app = {};

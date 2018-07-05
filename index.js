c06f382701dabc70d1ba11f108677a293b0b91f4
Jono Gilmour <j.gilmour@webtron.com.au>
2018-07-05 14:12:16 +1000

    Converted handlers.js to use ES6 class syntax. Split up handlers into their own self-contained files to ease maintenance. Also did some refactoring and cleanup.

--- index.js
+++ index.js
@@ -6,9 +6,6 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
-const data = require('./lib/data');
-
-data.read('checks', '2q62k151g4h02nggdzyt', (...args) => console.log(...args));
 
 // Declare the app
 const app = {};
